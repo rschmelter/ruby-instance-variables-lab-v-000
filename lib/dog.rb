ce14c76b4afb811b72d8434436626c34ebaa004f
Ryan Schmelter <rschmelter33@gmail.com>
2017-11-05 17:22:32 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,7 +2,7 @@ class Dog
   def name=(dog_name)
     @this_dogs_name = dog_name
   end
- 
+
   def name
     @this_dogs_name
   end
@@ -10,5 +10,5 @@ end
 
 lassie = Dog.new
 lassie.name = "Lassie"
- 
+
 puts lassie.name
